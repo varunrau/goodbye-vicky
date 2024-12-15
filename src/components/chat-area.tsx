3c050d1fe1901fd114ae2ccd0c8b85187d93dca0
Varun Rau <varunrau@gmail.com>
2024-12-15 13:51:29 -0500

    copy

--- src/components/chat-area.tsx
+++ src/components/chat-area.tsx
@@ -28,7 +28,7 @@ export function ChatArea() {
           rel="noopener noreferrer"
           className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-[#3F0E40] hover:bg-[#350D36] text-white h-10 px-8"
         >
-          Join Channel
+          Join channel to respond
         </a>
       </div>
     </div>
